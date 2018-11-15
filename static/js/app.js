9c927314ec8a606f28bfde5b5a35155f515d8173
STirso <kukuswish@gmail.com>
2018-11-14 20:28:55 -0500

    change filter function. add switch

--- static/js/app.js
+++ static/js/app.js
@@ -3,23 +3,27 @@ var tableData = data;
 var infoTable = d3.select("#ufo-table tbody");
 // YOUR CODE HERE!
 
-function filteredOutput(filBy=tableData){
+function filteredOutput(filBy=''){
 	var filData = [];
 
+	switch(filBy){
+		default:
+			filData=tableData;
+			break;
+	}
+	filData.forEach( (a,b) => {
+		var rows = infoTable.append('tr');
+		rows.append('td').html(a.datetime);
+		rows.append('td').html(a.city);
+		rows.append('td').html(a.state);
+		rows.append('td').html(a.country);
+		rows.append('td').html(a.shape);
+		rows.append('td').html(a.durationMinutes);
+		rows.append('td').html(a.comments);
+	});
+
+	console.log(filBy);
 }
-
-
-tableData.forEach( (a,b) => {
-	var rows = infoTable.append('tr');
-	rows.append('td').html(a.datetime);
-	rows.append('td').html(a.city);
-	rows.append('td').html(a.state);
-	rows.append('td').html(a.country);
-	rows.append('td').html(a.shape);
-	rows.append('td').html(a.durationMinutes);
-	rows.append('td').html(a.comments);
-});
-console.log(tableData);
 
 filteredOutput();
 
